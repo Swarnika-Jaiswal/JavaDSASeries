3af1d99470e2d7a6af97ce57ae197922a4725c86
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-06-09 09:28:13 +0530

    Add basic java programs

---
+++ src/Basic.java
@@ -0,0 +1,12 @@
+public class Basic {
+    public static void main(String[] args) {
+
+            System.out.println("Hello!");
+            System.out.println('A');
+            System.out.println(2 + 3);
+            System.out.println("2+3");
+            System.out.print("hey");
+            System.out.println("hii");
+
+    }
+}
