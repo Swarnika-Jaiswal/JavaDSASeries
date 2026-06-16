7afd35310386cc5a4c80eb101c994e081b2d3331
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-06-16 10:56:33 +0530

    Add methods practice questions in java.

---
+++ src/An_Methods_practice.java
@@ -0,0 +1,46 @@
+public class An_Methods_practice {
+    //    1.Create a method PRINTWELCOMEMESSAGE() that prints a greeting.
+    static void PRINTWELCOMEMESSAGE() {
+        System.out.println("WELCOME!");
+    }
+
+
+    //    2. Create a method ADD(int a, int b) that returns the sum.
+    static int ADD(int a, int b) {
+        int sum = a + b;
+        System.out.println("SUM=" + sum);
+        return sum;
+    }
+
+
+    //    3.Create a method ISEVEN(int number) that returns true if the number is even.
+    static boolean ISEVEN(int number) {
+        return number % 2 == 0;
+    }
+
+
+    //    4.Create a method GETMAXIMUM(int a, int b) that returns the larger number.
+    static int GETMAXIMUM(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+
+
+//    5.Create a method CALCULATEPERCENTAGE(int obt, int total).
+
+
+//    6. Create an overloaded method DISPLAY() with one int parameter or one string parameter.
+//    7. Create a method UPDATEVALUE(int x) and verify that original variable in main() does not change.
+    }
+
+
+    public static void main(String[] args) {
+        PRINTWELCOMEMESSAGE();
+        ADD(2, 4);
+        System.out.println(ISEVEN(4)); // true
+        System.out.println(ISEVEN(7)); // false
+
+    }
+}
