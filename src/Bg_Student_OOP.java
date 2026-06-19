622b9dded8f99f4587303705b853af5dc8ea52b1
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-06-19 22:07:52 +0530

    Add class and objects in java.

---
+++ src/Bg_Student_OOP.java
@@ -0,0 +1,19 @@
+public class Bg_Student_OOP {
+// Attributes
+    public int id;
+    public int age;
+    public String name;
+    public int nos;
+
+
+// Methods
+public void study(){
+    System.out.println(name + "Studying");
+}
+public void sleep(){
+    System.out.println(name + "Sleeping");
+}
+public void bunk(){
+    System.out.println(name + "Bunking");
+}
+}
