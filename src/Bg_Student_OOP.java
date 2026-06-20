b747da712c4b822034811755872dae1a2528db31
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-06-20 23:30:39 +0530

    Add some changes in program.

--- src/Bg_Student_OOP.java
+++ src/Bg_Student_OOP.java
@@ -4,6 +4,7 @@ public class Bg_Student_OOP {
     public int age;
     public String name;
     public int nos;
+    private String gf;
 
 //Constructor:
    //1.Default Constructor // attr -> garbage
@@ -11,12 +12,13 @@ public class Bg_Student_OOP {
         System.out.println("Student Default constructor called!");
     }
    //2.Parameterized Constructor
-    public Bg_Student_OOP(int id, int age, String name, int nos){
+    public Bg_Student_OOP(int id, int age, String name, int nos, String gf){
         System.out.println("Student Default constructor called!");
         this.id = id;
         this.age = age;
         this.name = name;
         this.nos = nos;
+        this.gf = gf;
 
     }
    // 3.Copy Constructor
@@ -40,5 +42,8 @@ public void sleep(){
 }
 public void bunk(){
     System.out.println(name + "Bunking");
+}
+private void gfChatting(){
+    System.out.println(name + "Chatting");
 }
 }
