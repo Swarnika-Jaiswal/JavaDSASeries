805d84ed31458e1acbee5c263cd52ef04496f849
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-01 12:49:46 +0530

    Add types of method

--- src/Basic_Concepts/Am_Methods.java
+++ src/Basic_Concepts/Am_Methods.java
@@ -15,6 +15,7 @@ public class Am_Methods {
             int ans = x+y;
             return ans;
         }
+
 //  2. Non Parameterized method:
         static int sub(){
             int a = 5;
@@ -22,14 +23,32 @@ public class Am_Methods {
             int ans = a-b;
             return ans;
         }
+
+//  3. Void method:(Parameterized/Non Parameterized)
+//      void method are the method that do not contain return.
+        static void mulipy(int x, int y){
+            int res = x*y;
+            System.out.println("multiplication = " + res);
+        }
+
+//  4. No Void method:(Parameterized/Non Parameterized)
+//      In this method we write the datatype in which it it retun the value along with retun statement.
+        static float div(){
+            int x = 5;
+            int y = 2;
+            float div = x/y;
+            return div;
+        }
 // program execution point is: main
         public static void main(String[] args){
             System.out.println("hi");
 // Method call
         print2kaTable();
         System.out.println("bye");
-        System.out.println(add(4,2));
-            System.out.println(sub());
+        System.out.println("addition = "+add(4,2));
+        System.out.println("subtraction = "+sub());
+        mulipy(2,8);
+        System.out.println("division = "+ div());
         }
 
     }
