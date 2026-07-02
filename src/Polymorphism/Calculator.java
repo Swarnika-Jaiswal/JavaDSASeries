6ed81ccfe8ca1c037d8044e7653e2ab83613b9e9
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-02 22:47:24 +0530

    Add method overloading

---
+++ src/Polymorphism/Calculator.java
@@ -0,0 +1,18 @@
+package Polymorphism;
+
+public class Calculator {
+
+//    Method Overloading
+    int add (int a, int b){
+        return a+b;
+    }
+
+    int add(int a, int b, int c){
+        return a+b+c;
+    }
+
+    float add(int a, int b , float c){
+        return a+b+c;
+    }
+
+}
