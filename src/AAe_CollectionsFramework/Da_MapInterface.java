4651fac890e087985cb88431c648bc3bb74d23b4
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-07 23:18:51 +0530

    Add  Map Interface in Java Collections Framework.

--- src/AAe_CollectionsFramework/Da_MapInterface.java
+++ src/AAe_CollectionsFramework/Da_MapInterface.java
@@ -1,7 +1,9 @@
 package AAe_CollectionsFramework;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Da_MapInterface {
     public static void main(String[] args) {
@@ -27,8 +29,46 @@ public class Da_MapInterface {
         System.out.println(m);
 
         System.out.println(m.size());
-        m.clear();
-        System.out.println(m.size());
+//        m.clear();
+//        System.out.println(m.size());
+
+        m.putIfAbsent("in", "India2");
+        System.out.println(m);
+
+        System.out.println(m.get("br"));
+
+        System.out.println(m.getOrDefault("u", "NONE"));
+
+        System.out.println(m.containsKey("im"));
+        System.out.println(m.containsValue("United States"));
+
+        System.out.println(m);
+        m.replace("in", "indonesia");
+        System.out.println(m);;
+
+        Set<String> keyset = m.keySet();
+        System.out.println(keyset);
+
+        Collection <String> valueSet =  m.values();
+        System.out.println(valueSet);
+
+//        get all the entries for now
+       Set<Map.Entry<String, String>> entryset =  m.entrySet();
+        System.out.println("Printing entries: "+ entryset);
+
+//        iteration over entrySet:
+        Map<Integer,String> map = new HashMap<>();
+        map.put(1, "One");
+        map.put(2,"Two");
+        map.put(3,"Three");
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()){
+            System.out.println("Key: " + entry.getKey()+", Value: "+ entry.getValue());
+        }
+
+
+
+
 
 
 
