22d3353192c4d73119c95acd791111ea48f703fa
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-07 23:20:39 +0530

    Add  Comparator in Java Collections Framework.

---
+++ src/AAe_CollectionsFramework/E_Comparator_Student.java
@@ -0,0 +1,41 @@
+package AAe_CollectionsFramework;
+
+public class E_Comparator_Student {
+
+    public int age;
+
+    public String name;
+
+    public int weight;
+
+    public E_Comparator_Student(int age, String name, int weight){
+        this.age = age;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
