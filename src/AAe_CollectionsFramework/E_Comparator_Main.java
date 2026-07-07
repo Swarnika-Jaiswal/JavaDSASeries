06fcbd1df6c66a19a6ca9e5cfbb80d244abbf058
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-07 23:19:54 +0530

    Add  Comparator in Java Collections Framework.

---
+++ src/AAe_CollectionsFramework/E_Comparator_Main.java
@@ -0,0 +1,15 @@
+package AAe_CollectionsFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class E_Comparator_Main {
+    public static void main(String[] args) {
+
+        List<E_Comparator_Student> students = new ArrayList<>();
+
+//        E_Comparator_Student.add(new S);
+
+
+    }
+}
