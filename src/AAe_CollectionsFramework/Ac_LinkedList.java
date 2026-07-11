45d0222122f9092e1e0e98d4ad0c6970e7153c43
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-11 23:49:48 +0530

    Add  Array Sort ZerosOnes problems.

---
+++ src/AAe_CollectionsFramework/Ac_LinkedList.java
@@ -0,0 +1,20 @@
+package AAe_CollectionsFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ac_LinkedList {
+    public static void main(String[] args) {
+        //    ArrayList -> concrete class
+    List<Integer> list = new ArrayList<>();
+//    Collection<Integer> collection = new ArrayList<>();
+//      List<Integer> list = new ArrayList<>();
+
+//      add values in list:
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+    }
+}
