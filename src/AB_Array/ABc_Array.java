e9ea00f28b3093a74000bdd2e0838c4680d6dff7
Swarnika Jaiswal <swarnikajaiswal584@gmail.com>
2026-07-12 23:55:10 +0530

    Add two sum in an array.

---
+++ src/AB_Array/ABc_Array.java
@@ -0,0 +1,24 @@
+package AB_Array;
+
+public class ABc_Array {
+    public static int[] twoSum(int[] nums, int target){
+        int n=nums.length;
+        for (int i=0; i<n-1; i++){
+            for (int j=i+1; j<n; j++){
+                if (nums[i] + nums[j] == target){
+                    int ans[] ={i,j};
+                    return ans;
+                }
+            }
+        }
+        int ans[] = {};
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2,7,11,15};
+        int res[] = twoSum(arr,9);
+//        System.out.println(res[0],res[1]);
+    }
+
+}
